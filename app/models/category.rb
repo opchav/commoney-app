e2f8c105b9f7e7c35664fdb7abf1c16e0eae4f36
Paulo Chaves <opaulochaves@gmail.com>
2021-12-16 16:50:29 -0300

    add type to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
+
+  validates :category_type, inclusion: { in: %w[income expense] }
+  validates :color, inclusion: { in: %w[red green yellow blue silver gray orange] }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_194300) do
+ActiveRecord::Schema.define(version: 2021_12_16_194743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_12_16_194300) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category_type"
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
